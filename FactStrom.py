f7a2b8beb78dbf5461ac02303ba7d27a1654da7f
Fatemeh <fatemeh.ch.official@gmail.com>
2025-10-30 16:43:42 +0330

    List of questions added to FactStorm.py

---
+++ FactStrom.py
@@ -0,0 +1,23 @@
+#Questions and their answers
+Questions_list={
+    "What is the largest planet in our solar system?": "jupiter",
+    "Who painted the Mona Lisa?": "leonardo da vinci",
+    "What is the capital city of Australia?": "canberra",
+    "Which ocean is the deepest on Earth?": "pacific",
+    "How many bones are in the adult human body?": "206",
+    "What is the hardest natural substance on Earth?": "diamond",
+    "Which country is known as the Land of the Rising Sun?": "japan",
+    "Who wrote the play 'Hamlet'?": "william shakespeare",
+    "What is the chemical symbol for water?": "h2o",
+    "Which planet is closest to the Sun?": "mercury",
+    "What is the largest mammal in the world?": "blue whale",
+    "In what year did man first land on the moon?": "1969",
+    "Which animal is known for its black and white stripes?": "zebra",
+    "What is the smallest prime number?": "2",
+    "Which element has the chemical symbol 'O'?": "oxygen",
+    "Who invented the telephone?": "alexander graham bell",
+    "How many continents are there on Earth?": "7",
+    "What is the capital city of Canada?": "ottawa",
+    "Which bird is known for its colorful tail feathers?": "peacock",
+    "Which language has the most native speakers worldwide?": "mandarin"
+}
